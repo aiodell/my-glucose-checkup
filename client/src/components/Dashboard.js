a872485f6b003b8fc6948d7b0519be3d2addfa42
Andrianna <andrianna.odell@gmail.com>
2022-09-26 15:22:18 -0400

    base dashboard display

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
-import {useHistory, useParams } from "react-router-dom"
+import {useHistory, useParams, Link } from "react-router-dom"
 
-const Dashboard = ({readings, setReadings}) => {
+const Dashboard = ({readings, setReadings, currentUser}) => {
 	const[errors, setErrors] = useState([])
 	const params = useParams()
 	const history= useHistory()
@@ -20,24 +20,54 @@ const Dashboard = ({readings, setReadings}) => {
 		})
 	}, [])
 
+	// sort the highest, lowest, and most current value
 	const lowestValue = [...readings].sort((a,b) => a.value - b.value)
 	const highestValue = [...readings].sort((a,b) => b.value - a.value)
 	const highestId =[...readings].sort((a,b) => b.id - a.id)
 
-	const renderReadings = readings.map((bgl) => {
+	const renderLowest = lowestValue.slice(0,1).map((num) => {
 		return(
-				<p key ={bgl.id}>
-					{bgl.value}
-				</p>
+			<h2 key={num.id}>
+				Lowest Reading: {num.value}
+			</h2>
+		)	
+	})
+
+	const renderHighest = highestValue.slice(0,1).map((num)=> {
+		return(
+			<h2 key={num.id}>
+				Highest Reading: {num.value}
+			</h2>
 		)
 	})
 
+	const renderCurrent = highestId.slice(0,1).map((num) => {
+		return(
+		<div>
+			<h1 key={num.id}> {num.value} </h1>
+			<Link to= {`/bgls/${num.id}`}> View Details</Link>
+		</div>
+)
+	})
+ 
 	const toReadings = () => {history.push("/bgls/all")}
+	const newReading = () => {history.push("/bgls/new")}
 
 	return(
 		<div>
-			<button onClick={toReadings}>All Readings</button>
-			{renderReadings}
+			<div>
+				<h1>Welcome to your dashboard, {currentUser.first_name} </h1>
+			<div>
+				{renderCurrent}
+			</div>
+			</div>
+			{renderLowest}
+			{renderHighest}
+			<div>
+				<button onClick={toReadings}>All Readings</button>
+				<button onClick={newReading}>New Reading</button>
+			</div>
+			
 		</div>
 			
 	)
